4e4cfde5e3f01f184a255dcabcd6990c3da5f7cc
David Kim <dkim@gitlab.com>
2020-11-05 22:55:27 +1030

    Add applicable_approval_rules to reviewer entity

--- ee/app/models/ee/merge_request.rb
+++ ee/app/models/ee/merge_request.rb
@@ -274,6 +274,12 @@ module EE
       (base_pipeline.security_scans.pluck(:scan_type) - actual_head_pipeline.security_scans.pluck(:scan_type)).uniq
     end
 
+    def applicable_approval_rules_for_user(user)
+      approval_rules.applicable_to_branch(target_branch).select do |rule|
+        rule.approvers.map(&:id).include?(user.id)
+      end
+    end
+
     private
 
     def has_approved_license_check?
--- ee/app/serializers/ee/merge_request_reviewer_entity.rb
+++ ee/app/serializers/ee/merge_request_reviewer_entity.rb
@@ -6,6 +6,9 @@ module EE
 
     prepended do
       expose :gitlab_employee?, as: :is_gitlab_employee, if: proc { ::Gitlab.com? && ::Feature.enabled?(:gitlab_employee_badge) }
+      expose :applicable_approval_rules, using: ::EE::API::Entities::ApprovalRuleShort do |user, options|
+        options[:merge_request]&.applicable_approval_rules_for_user(user)
+      end
     end
   end
 end
