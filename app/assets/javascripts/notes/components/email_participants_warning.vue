9dd0e18c8c171b55787f007c4565a79f59b9e8f8
Lee Tickett <lee@tickett.net>
2020-11-23 19:04:29 +0000

    Starting to handle

--- app/assets/javascripts/notes/components/email_participants_warning.vue
+++ app/assets/javascripts/notes/components/email_participants_warning.vue
@@ -8,12 +8,43 @@ export default {
       type: Array,
       required: true,
     },
+    numberOfLessParticipants: {
+      type: Number,
+      required: false,
+      default: 3,
+    },
+  },
+  data() {
+    return {
+      isShowingMoreParticipants: false,
+    };
   },
   computed: {
     title() {
       return sprintf(__('%{emails} will be notified of your comment.'), {
         emails: toNounSeriesText(this.emails),
       });
+    },
+    lessParticipants() {
+      return this.emails.slice(0, this.numberOfLessParticipants);
+    },
+    visibleParticipants() {
+      return this.isShowingMoreParticipants ? this.emails : this.lessParticipants;
+    },
+    hasMoreParticipants() {
+      return this.participants.length > this.numberOfLessParticipants;
+    },
+    toggleLabel() {
+      let label = '';
+      if (this.isShowingMoreParticipants) {
+        label = __('- show less');
+      } else {
+        label = sprintf(__('+ %{moreCount} more'), {
+          moreCount: this.participants.length - this.numberOfLessParticipants,
+        });
+      }
+
+      return label;
     },
   },
 };
