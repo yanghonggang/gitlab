7a0eba28d04f3c7d8e3196e099b288f87aff72f6
Doug Stull <dstull@gitlab.com>
2020-11-24 12:47:31 -0500

    Refine current group creation form
    - collapse some items and move out in prep
      for inviting members.

--- qa/qa/page/group/new.rb
+++ qa/qa/page/group/new.rb
@@ -7,6 +7,9 @@ module QA
         view 'app/views/shared/_group_form.html.haml' do
           element :group_path_field, 'text_field :path' # rubocop:disable QA/ElementWithPattern
           element :group_name_field, 'text_field :name' # rubocop:disable QA/ElementWithPattern
+        end
+
+        view 'app/views/shared/_group_form_description.html.haml' do
           element :group_description_field, 'text_area :description' # rubocop:disable QA/ElementWithPattern
         end
 
