93d850adbbf40eccc20b91dc344bfd7b1ec4f2f6
David O'Regan <doregan@gitlab.com>
2020-11-26 10:34:26 +0000

    Apply Merge Feedback

---
+++ ee/app/assets/javascripts/oncall_schedules/constants.js
@@ -0,0 +1,15 @@
+export const LENGTH_ENUM = {
+  hours: 'hours',
+  days: 'days',
+  weeks: 'weeks',
+};
+
+export const CHEVRON_SKIPPING_SHADE_ENUM = ['500', '600', '700', '800', '900', '950'];
+
+export const CHEVRON_SKIPPING_PALETTE_ENUM = [
+  'gl-bg-data-viz-blue-',
+  'gl-bg-data-viz-orange-',
+  'gl-bg-data-viz-aqua-',
+  'gl-bg-data-viz-green-',
+  'gl-bg-data-viz-magenta-',
+];
