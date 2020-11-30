b42352d71f48c119bfd18151f33622b1970b1033
Sarah GP <sarah.groff.palermo@gmail.com>
2020-11-30 13:16:33 +0100

    Add new graph and apply to component

--- app/assets/javascripts/pipelines/components/graph/graph_component.vue
+++ app/assets/javascripts/pipelines/components/graph/graph_component.vue
@@ -1,10 +1,12 @@
 <script>
+import LinkedGraphWrapper from '../graph_shared/linked_graph_wrapper.vue';
 import StageColumnComponent from './stage_column_component.vue';
 import { MAIN } from './constants';
 
 export default {
   name: 'PipelineGraph',
   components: {
+    LinkedGraphWrapper,
     StageColumnComponent,
   },
   props: {
@@ -36,13 +38,35 @@ export default {
       class="gl-pipeline-min-h gl-display-flex gl-position-relative gl-overflow-auto gl-bg-gray-10 gl-white-space-nowrap"
       :class="{ 'gl-py-5': !isLinkedPipeline }"
     >
-      <stage-column-component
-        v-for="stage in graph"
-        :key="stage.name"
-        :title="stage.name"
-        :groups="stage.groups"
-        :action="stage.status.action"
-      />
+      <linked-graph-wrapper>
+        <template #upstream>
+          <linked-pipelines-column
+            v-if="showUpstreamPipelines"
+            :linked-pipelines="upstreamPipelines"
+            :column-title="__('Upstream')"
+            :type="$options.pipelineTypeConstants.UPSTREAM"
+          />
+        </template>
+        <template #main>
+          <stage-column-component
+            v-for="stage in graph"
+            :key="stage.name"
+            :title="stage.name"
+            :groups="stage.groups"
+            :action="stage.status.action"
+          />
+        </template>
+        <template #downstream>
+          <linked-pipelines-column
+            v-if="showDownstreamPipelines"
+            :linked-pipelines="downstreamPipelines"
+            :column-title="__('Downstream')"
+            :type="$options.pipelineTypeConstants.DOWNSTREAM"
+            @downstreamHovered="setJob"
+            @pipelineExpandToggle="setPipelineExpanded"
+          />
+        </template>
+      </linked-graph-wrapper>
     </div>
   </div>
 </template>
