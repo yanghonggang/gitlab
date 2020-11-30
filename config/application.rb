473f413fd1ccf35958ac2543a5cb57d6c711a10c
Savas Vedova <svedova@gitlab.com>
2020-11-30 17:17:47 +0300

    Move CSS to page related bundle
    Security dashboard css is very specific to its own page
    therefore we've decided to move it into its own bundle
    in order to improve page performance

--- config/application.rb
+++ config/application.rb
@@ -196,6 +196,7 @@ module Gitlab
     config.assets.precompile << "page_bundles/pipelines.css"
     config.assets.precompile << "page_bundles/pipeline_schedules.css"
     config.assets.precompile << "page_bundles/productivity_analytics.css"
+    config.assets.precompile << "page_bundles/security_dashboard.css"
     config.assets.precompile << "page_bundles/terminal.css"
     config.assets.precompile << "page_bundles/todos.css"
     config.assets.precompile << "page_bundles/reports.css"
