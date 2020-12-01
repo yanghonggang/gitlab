e7e21f75ef4af36e51f415777bb5768efd4b11ac
David O'Regan <doregan@gitlab.com>
2020-12-01 12:32:04 +0000

    Fix linitng failures

--- ee/app/assets/javascripts/oncall_schedules/constants.js
+++ ee/app/assets/javascripts/oncall_schedules/constants.js
@@ -6,10 +6,4 @@ export const LENGTH_ENUM = {
 
 export const CHEVRON_SKIPPING_SHADE_ENUM = ['500', '600', '700', '800', '900', '950'];
 
-export const CHEVRON_SKIPPING_PALETTE_ENUM = [
-  'gl-bg-data-viz-blue-',
-  'gl-bg-data-viz-orange-',
-  'gl-bg-data-viz-aqua-',
-  'gl-bg-data-viz-green-',
-  'gl-bg-data-viz-magenta-',
-];
+export const CHEVRON_SKIPPING_PALETTE_ENUM = ['blue', 'orange', 'aqua', 'green', 'magenta'];
