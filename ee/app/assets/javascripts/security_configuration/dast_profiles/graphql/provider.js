21112235ead4deb12a96e0e78f9bc77419e075bb
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2020-12-01 11:11:39 -0500

    Remove client-side GraphQL mocks

--- ee/app/assets/javascripts/security_configuration/dast_profiles/graphql/provider.js
+++ ee/app/assets/javascripts/security_configuration/dast_profiles/graphql/provider.js
@@ -1,36 +1,14 @@
 import Vue from 'vue';
 import VueApollo from 'vue-apollo';
-import { random } from 'lodash';
 import createDefaultClient from '~/lib/graphql';
 
 Vue.use(VueApollo);
 
-const resolvers = {
-  Query: {
-    dastSiteValidations: (_, { normalizedTargetUrls }) => {
-      return {
-        nodes: normalizedTargetUrls.map(url => {
-          const randNumber = random(100);
-          let validationStatus = 'INPROGRESS_VALIDATION';
-          if (randNumber < 20) {
-            validationStatus = 'PASSED_VALIDATION';
-          } else if (randNumber > 80) {
-            validationStatus = 'FAILED_VALIDATION';
-          }
-          return {
-            normalizedTargetUrl: url,
-            status: validationStatus,
-            __typename: 'DastSiteValidation',
-          };
-        }),
-        __typename: 'DastSiteValidations',
-      };
-    },
-  },
-};
-
 export default new VueApollo({
-  defaultClient: createDefaultClient(resolvers, {
-    assumeImmutableResults: true,
-  }),
+  defaultClient: createDefaultClient(
+    {},
+    {
+      assumeImmutableResults: true,
+    },
+  ),
 });
