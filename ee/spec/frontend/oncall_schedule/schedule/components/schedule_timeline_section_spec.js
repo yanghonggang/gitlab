4d1d26bff737bda57a309f855325a784057d02d1
David O'Regan <doregan@gitlab.com>
2020-12-02 11:20:42 +0000

    Revert "Add rotation button to schedules"
    This reverts commit d2a63572eb805164f2ac7f46733e208ea0138d2b.

--- ee/spec/frontend/oncall_schedule/schedule/components/schedule_timeline_section_spec.js
+++ ee/spec/frontend/oncall_schedule/schedule/components/schedule_timeline_section_spec.js
@@ -1,8 +1,5 @@
 import { shallowMount } from '@vue/test-utils';
-import { GlCard, GlButton } from '@gitlab/ui';
-import ScheduleTimelineSection, {
-  i18n,
-} from 'ee/oncall_schedules/components/schedule/components/schedule_timeline_section.vue';
+import ScheduleTimelineSection from 'ee/oncall_schedules/components/schedule/components/schedule_timeline_section.vue';
 import WeeksHeaderItem from 'ee/oncall_schedules/components/schedule/components/preset_weeks/weeks_header_item.vue';
 import { getTimeframeForWeeksView } from 'ee/oncall_schedules/components/schedule/utils';
 import { PRESET_TYPES } from 'ee/oncall_schedules/components/schedule/constants';
@@ -12,9 +9,6 @@ describe('RoadmapTimelineSectionComponent', () => {
   const mockTimeframeInitialDate = new Date(2018, 0, 1);
   const mockTimeframeWeeks = getTimeframeForWeeksView(mockTimeframeInitialDate);
 
-  const findRotations = () => wrapper.find(GlCard);
-  const findAddRotation = () => wrapper.find(GlButton);
-
   function mountComponent({
     presetType = PRESET_TYPES.WEEKS,
     timeframe = mockTimeframeWeeks,
@@ -23,10 +17,6 @@ describe('RoadmapTimelineSectionComponent', () => {
       propsData: {
         presetType,
         timeframe,
-      },
-      stubs: {
-        GlCard,
-        GlButton,
       },
     });
   }
@@ -43,7 +33,7 @@ describe('RoadmapTimelineSectionComponent', () => {
   });
 
   it('renders component container element with class `timeline-section`', () => {
-    expect(wrapper.html()).toContain('timeline-section');
+    expect(wrapper.classes()).toContain('timeline-section');
   });
 
   it('renders empty header cell element with class `timeline-header-blank`', () => {
@@ -52,14 +42,5 @@ describe('RoadmapTimelineSectionComponent', () => {
 
   it('renders weeks header items based on timeframe data', () => {
     expect(wrapper.findAll(WeeksHeaderItem).length).toBe(mockTimeframeWeeks.length);
-  });
-
-  it('renders the rotation card wrapper', () => {
-    expect(findRotations().exists()).toBe(true);
-  });
-
-  it('renders the add rotation button in the rotation card wrapper', () => {
-    expect(findAddRotation().exists()).toBe(true);
-    expect(findAddRotation().text()).toBe(i18n.addARotation);
   });
 });
