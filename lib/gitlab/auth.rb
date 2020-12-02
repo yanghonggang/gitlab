439f98cb15753651d35cd323a2552444d5de2aca
serenafang <sfang@gitlab.com>
2020-12-02 00:32:43 +0000

    Authenticate if can access api

--- lib/gitlab/auth.rb
+++ lib/gitlab/auth.rb
@@ -200,7 +200,7 @@ module Gitlab
 
         return unless valid_scoped_token?(token, all_available_scopes)
 
-        if token.user.project_bot? || token.user.can?(:log_in)
+        if token.user.can?(:access_api)
           Gitlab::Auth::Result.new(token.user, nil, :personal_access_token, abilities_for_scopes(token.scopes))
         end
       end
